cb1fb95f2d7f80959374a368661e511428593d81
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-11-14 00:35:38 +0100

    cleaned level 1 files.

--- level-1/encrypt.py
+++ level-1/encrypt.py
@@ -1,32 +1,13 @@
-import regex
-import regex as re
-import string
+def solution(x):
+    abc = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_message = ''
+    for char in x:
+        if char in abc:
+            encrypted_message += abc[::-1][abc.index(char)]
+        else:
+            encrypted_message += char
+
+    return encrypted_message
 
 
-class solution():
-    def solution(x):
-        # Every lowercase letter in [a...z] is replaced with their corresponding
-        # one in [z...a]. Every other character, including uppercase letters and
-        # punctuation is untouched.
-
-        # Input:
-        # solution.solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?")
-        # Output:
-        #     did you see last night's episode?
-
-        #
-        re_lowercase = re.compile('[a-z]')
-        abc = string.ascii_lowercase
-        translator = str.maketrans(abc, abc[::-1], ' ')
-        encrypted_message = ''
-
-        for char in x:
-            if regex.match(re_lowercase, char):
-                encrypted_message += char.translate(translator)
-            else:
-                encrypted_message += char
-
-        return encrypted_message
-
-
-print(solution.solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?"))
+print(solution('Hello!'))
